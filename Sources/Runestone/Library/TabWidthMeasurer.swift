16906323d2e44e493eeeb801b9d514b5bdd112ea
Pasi Salenius <pasi@freshbits.fi>
2025-11-22 15:32:32 +0200

    Make TabWidthMeasurer multiplatform.

--- Sources/Runestone/Library/TabWidthMeasurer.swift
+++ Sources/Runestone/Library/TabWidthMeasurer.swift
@@ -1,10 +1,20 @@
+#if os(macOS)
+import AppKit
+#endif
+#if os(iOS)
 import UIKit
+#endif
 
 enum TabWidthMeasurer {
-    static func tabWidth(tabLength: Int, font: UIFont) -> CGFloat {
+    static func tabWidth(tabLength: Int, font: MultiPlatformFont) -> CGFloat {
         let str = String(repeating: " ", count: tabLength)
         let maxSize = CGSize(width: CGFloat.greatestFiniteMagnitude, height: .greatestFiniteMagnitude)
+        #if os(macOS)
+        let options: NSString.DrawingOptions = [.usesFontLeading, .usesLineFragmentOrigin]
+        #endif
+        #if os(iOS)
         let options: NSStringDrawingOptions = [.usesFontLeading, .usesLineFragmentOrigin]
+        #endif
         let attributes: [NSAttributedString.Key: Any] = [.font: font]
         let bounds = str.boundingRect(with: maxSize, options: options, attributes: attributes, context: nil)
         return round(bounds.size.width)
