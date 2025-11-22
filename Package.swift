b1c2f687c0542de66f7ad34b7c176816f5f7eb94
Pasi Salenius <pasi@freshbits.fi>
2025-11-22 16:22:14 +0200

    Reformat Package.swift file. Remove empty file from project.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,9 @@ import PackageDescription
 let package = Package(
     name: "Runestone",
     defaultLocalization: "en",
-    platforms: [.iOS(.v14), .macOS(.v11)],
+    platforms: [
+        .iOS(.v14), .macOS(.v11)
+    ],
     products: [
         .library(name: "Runestone", targets: ["Runestone"])
     ],
